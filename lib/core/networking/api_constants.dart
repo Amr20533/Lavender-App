2e92a58a73af6e1f3d216507cb351eb9e89ed8cf
Amr20533 <amrelnemr638@gmail.com>
2026-01-02 19:13:26 +0200

    Chat

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -2,10 +2,10 @@ class ApiConstants {
   // static const String baseUrl = "http://192.168.1.8:8000/api/v1/user";
   //  static const String baseUrl = "http://192.168.1.5:8000/";
   //  static const String imagePath="http://192.168.1.5:8000";
-  static const String baseUrl = "http://10.0.2.2:8000/";
-  static const String imagePath = "http://10.0.2.2:8000";
-  // static const String baseUrl = "https://lavender26.pythonanywhere.com/";
-  // static const String imagePath = "https://lavender26.pythonanywhere.com";
+  // static const String baseUrl = "http://10.0.2.2:8000/";
+  // static const String imagePath = "http://10.0.2.2:8000";
+  static const String baseUrl = "https://lavender26.pythonanywhere.com/";
+  static const String imagePath = "https://lavender26.pythonanywhere.com";
   static const String signUp = "api/v1/user/register";
   static const String signIn = "api/v1/user/login";
 
